8aebdd498a0e0d91ba81b92a067e2e19107511cf
OlguisT <olgseb@gmail.com>
2017-09-18 14:23:46 -0700

    prueba

--- admin/Collector.php
+++ admin/Collector.php
@@ -3,11 +3,15 @@ include_once('dataBase.php');
 
 // Define configuration
 
+define("DB_HOST", "ec2-54-163-230-219.compute-1.amazonaws.com");
+define("DB_USER", "pgmfrjwdtzjwrp");
+define("DB_PASS", "2d72d0f3411301ef353eb28da736b72321048f7e4f6c53ff984af71813879f3b");
+define("DB_NAME", "dftgqjbnucrk1o");
 
-define("DB_HOST", "localhost");
-define("DB_USER", "postgres");
-define("DB_PASS", "postgres");
-define("DB_NAME", "comunidad");
+//define("DB_HOST", "localhost");
+//define("DB_USER", "postgres");
+//define("DB_PASS", "postgres");
+//define("DB_NAME", "comunidad");
 
 
 
